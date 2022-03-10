47ada2bb72744f5d577303254424fce7db01446f
zayanahmed925 <rjraju925@gmail.com>
2022-03-10 22:08:02 +0600

    Report display problem solved

--- js/app.js
+++ js/app.js
@@ -136,7 +136,6 @@ const createPost = (post) => {
 const showPosts = (posts) => {
   const productsContainer = document.getElementById("posts");
   productsContainer.innerHTML = "";
-
   posts.forEach((post) => {
     const div = createPost(post);
     productsContainer.appendChild(div);
@@ -153,7 +152,9 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  document.getElementById("reported").innerHTML = '';
+  // console.log(reportedPosts)
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
